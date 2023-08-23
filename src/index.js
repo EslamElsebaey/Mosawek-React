0ce2268aa0543338637d04a08dbd165dd38eab95
EslamElsebaey <eslamelsebaey.11@gmail.com>
2023-08-23 21:29:16 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
      <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
